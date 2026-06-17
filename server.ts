e1a4a47ecbb21505162e4b2f608609b29a60f2d2
mariofalluna-spec <mariofal.luna@gmail.com>
2026-06-17 01:12:40 -0500

    Add files via upload

--- server.ts
+++ server.ts
@@ -8,7 +8,7 @@ import { supabaseClient } from "./src/db/supabaseClient.ts";
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
